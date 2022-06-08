ba04b96a1da8aa7b28b81f5403024bb6d19f338f
Piyumi047 <dulanjaligedarawtta@gmail.com>
2022-06-09 00:23:39 +0530

    filter   card interface done

--- carhub/force-app/main/default/lwc/carFilter/carFilter.js
+++ carhub/force-app/main/default/lwc/carFilter/carFilter.js
@@ -1,3 +1,51 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import {getObjectInfo, getPicklistValues} from 'lightning/uiObjectInfoApi'
+import CAR_OBJECT from '@salesforce/schema/Car__c'
 
-export default class CarFilter extends LightningElement {}
+//car Schema
+import CATEGORY__FIELD from '@salesforce/schema/Car__c.Category__c'
+import MAKE_FIELD  from '@salesforce/schema/Car__c.Make__c'
+
+//constant
+const CATEGORY_ERROR="Error loading categoies"
+const MAKE_ERROR="Error loading make types"
+
+export default class CarFilter extends LightningElement {
+    filters={
+        seachKey:'',
+        maxPrice:999999
+    }
+    
+    categoryError=CATEGORY_ERROR
+    makeError=MAKE_ERROR
+
+    /*** fetching category*/
+     
+    @wire(getObjectInfo,{objectApiName:CAR_OBJECT})
+    carObjectInfo
+
+    @wire(getPicklistValues,{
+        recordTypeId:'$carObjectInfo.data.defaultRecordTypeId', 
+        fieldApiName:CATEGORY__FIELD
+     })categories
+
+     /*** fetching Make picklist */
+     @wire(getPicklistValues,{
+        recordTypeId:'$carObjectInfo.data.defaultRecordTypeId', 
+        fieldApiName:MAKE_FIELD
+     })makeType
+
+     /**search key handler */
+    handleSearchKeyChange(event){
+        console.log(event.target.value)
+        this.filters={...this.filters,"seachKey":event.target.value}
+    }
+
+    handleMaxPriceChange(){
+
+    }
+
+    handleCheckbox(){
+
+    }
+} 
